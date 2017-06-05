aa9e93b8cd707d285c8386c6077b12ef89f0f070
Radhika Sivarajan <radhikaraj333@gmail.com>
2017-06-05 10:36:38 -0700

    Scrollng, links etc

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,7 +1,44 @@
-$(document).ready(function() {
-    $("#menu-toggle").click(function(e) {
+$(document).ready(function () {
+    $("#menu-toggle").click(function (e) {
         e.preventDefault();
         $("#sidebar-header").toggleClass("toggled");
         $("#main-container").toggleClass("toggled");
     });
+
+    $(document).on("scroll", onScroll);
+
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top + 2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+
+    function onScroll(event) {
+        var scrollPos = $(document).scrollTop();
+        $('.sidebar-navigation ul li a').each(function () {
+            var currLink = $(this);
+            var refElement = $(currLink.attr("href"));
+            if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+                $('.sidebar-navigation ul li a').removeClass("active");
+                currLink.addClass("active");
+            }
+            else {
+                currLink.removeClass("active");
+            }
+        });
+    }
 });
