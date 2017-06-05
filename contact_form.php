e16dc76c33e28da0b06ea94471deb8bb309d6b1d
Radhika Sivarajan <radhikaraj333@gmail.com>
2017-06-05 15:43:38 -0700

    Form submit and handling

---
+++ contact_form.php
@@ -0,0 +1,33 @@
+<?php
+$to = "radhikaraj333@gmail.com.com";
+$subject = "HTML email";
+
+$message = "
+<html>
+<head>
+<title>HTML email</title>
+</head>
+<body>
+<p>This email contains HTML Tags!</p>
+<table>
+<tr>
+<th>Firstname</th>
+<th>Lastname</th>
+</tr>
+<tr>
+<td>John</td>
+<td>Doe</td>
+</tr>
+</table>
+</body>
+</html>
+";
+
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+$headers .= 'From: <webmaster@example.com>' . "\r\n";
+$headers .= 'Cc: myboss@example.com' . "\r\n";
+
+mail($to,$subject,$message,$headers);
+header('Location: index.html');
+?>
