7a4b18ed37688e8d7d3c97937034ec94ea301261
Radhika Sivarajan <radhikaraj333@gmail.com>
2017-06-05 17:27:55 -0700

    Form and fonts.

--- contact_form.php
+++ contact_form.php
@@ -1,19 +1,18 @@
 <?php
-$to      = 'radhikaraj333@gmail.com';
-$subject = 'the subject';
-$message = 'hello';
+if (isset($_POST["submit"])) {
+    $to      = 'radhikaraj333@gmail.com';
+    $subject = 'the subject';
+    $message = 'hello';
+    $header = "From: noreply@example.com\r\n";
+    $header.= "MIME-Version: 1.0\r\n";
+    $header.= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+    $header.= "X-Priority: 1\r\n";
 
-$header = "From: noreply@example.com\r\n";
-$header.= "MIME-Version: 1.0\r\n";
-$header.= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-$header.= "X-Priority: 1\r\n";
-
-$status = mail($to, $subject, $message, $headers);
-
-if($status)
-{
-    header('Location: index.html');
-} else {
-
+    $status = mail($to, $subject, $message, $headers);
+    if($status){
+        header('Location: index.html');
+    } else {
+        echo "Error";
+    }
 }
 ?>
