9fc65ba59272ff64ea02695d459eb97bb0290b5b
Radhika Sivarajan <radhikaraj333@gmail.com>
2017-08-01 22:54:56 -0700

    Styles

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -52,6 +52,6 @@ $(document).ready(function () {
         });
     }
     $(function () {
-        $('[data-toggle="tooltip"]').tooltip()
+        $('[data-toggle="tooltip"]').tooltip();
     })
 });
